2594eee6e705c3b2081d0e377b19dbc39cfaf5ba
Asim khan <asimabas95@gmail.com>
2025-03-28 04:40:13 +0500

    add firebase

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,13 +3,23 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAwVsZ10ZCofVxPPl1M04tEGVlvryXsCSk",
+//   authDomain: "esp32saeed-3e1fa.firebaseapp.com",
+//   projectId: "esp32saeed-3e1fa",
+//   storageBucket: "esp32saeed-3e1fa.appspot.com",
+//   messagingSenderId: "557873247282",
+//   appId: "1:557873247282:web:fbbebeb3e221f8dfbf782b",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAwVsZ10ZCofVxPPl1M04tEGVlvryXsCSk",
-  authDomain: "esp32saeed-3e1fa.firebaseapp.com",
-  projectId: "esp32saeed-3e1fa",
-  storageBucket: "esp32saeed-3e1fa.appspot.com",
-  messagingSenderId: "557873247282",
-  appId: "1:557873247282:web:fbbebeb3e221f8dfbf782b",
+  apiKey: "AIzaSyCC8eMwAB-H9SWV_tNaXmrkqm61Xu0ImiI",
+  authDomain: "campus-app-5c8d1.firebaseapp.com",
+  projectId: "campus-app-5c8d1",
+  storageBucket: "campus-app-5c8d1.firebasestorage.app",
+  messagingSenderId: "171794245736",
+  appId: "1:171794245736:web:67c7c2d0fe6e20a0b773dd",
+  measurementId: "G-2EEKHZGSDN",
 };
 
 const app = initializeApp(firebaseConfig);
